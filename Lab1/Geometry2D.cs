efb9a7b6bf8c4cc2cc518a8ec7b6e63f44bceca0
Dhruvkaneriya <dhruvkaneriya5@gmail.com>
2021-09-23 11:37:38 -0400

    Initial commit

---
+++ Lab1/Geometry2D.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    namespace Geometry2D
+    {
+        public class Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+
+            public Point(double x = 0, double y = 0)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public void GetCoords(out double x, out double y)
+            {
+                x = X;
+                y = Y;
+            }
+
+            public void SetCoords(double? x, double? y)
+            {
+                X = x ?? X;
+                Y = y ?? Y;
+            }
+
+            public override string ToString() => $"({X}, {Y})";
+        }
+
+        public class Vector
+        {
+            public double DX { get; private set; }
+            public double DY { get; private set; }
+
+            public Vector(double dx = 0, double dy = 0)
+            {
+                Set(dx, dy);
+            }
+
+            public void Set(double dx, double dy)
+            {
+                if (dx <= -1 && dy >= 1 || dx >= 1 && dy <= -1)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid argument because vector value is out of [-1,1]");
+                }
+            }
+
+            public override string ToString() => $"({DX}, {DY})";
+        }
+
+        public class Rectangle
+        {
+            public double Length { get; }
+            public double Width { get; }
+            public double Area { get; }
+
+            public Rectangle(double length, double width)
+            {
+
+            }
+        }
+    }
+}
